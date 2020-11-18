33f3e212e5b19f8b48ee82009411a1f18fec4e7f
ExPxHx <74666266+ExPxHx@users.noreply.github.com>
2020-11-18 12:57:31 -0300

    Initial commit

---
+++ app/src/main/java/com/derflus/streamradio/DataManager.java
@@ -0,0 +1,52 @@
+package com.derflus.streamradio;
+
+import android.content.Context;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import com.derflus.radio.R;
+
+/**
+ * Created by Mark on 2014.08.06..
+ */
+public class DataManager {
+
+    private Context context;
+
+    public DataManager(Context context) {
+        this.context = context;
+    }
+
+    public void createRadioListForRadioScreen(TableLayout UIRadioList, ArrayList<String> userRadios, TextView radioListName, TextView radioListLocation) {
+        ArrayList<RadioListElement> radioArray = new ArrayList<RadioListElement>();
+        radioArray.add(new RadioListElement(context, context.getResources().getString(R.string.radio_name), context.getResources().getString(R.string.radio_location), context.getResources().getString(R.string.radio_url)));
+        UIRadioList.removeAllViews();
+        RadioList radioList = new RadioList(context, radioArray, UIRadioList);
+        radioList.addRadioStations(radioListName, radioListLocation);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
